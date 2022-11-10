797cf523565b21640a9be712770c91a902eb1167
Adam <leftoverenchilada@gmail.com>
2022-11-09 23:59:57 -0500

    Rename

--- src/Application.cpp
+++ src/Application.cpp
@@ -1,12 +1,14 @@
 #include <filesystem>
 #include <iostream>
 #include "nvml.h"
-#include "Windows.h"
+#include <Urlmon.h>
+#include <Windows.h>
 
 using namespace std;
 
 float driverVersion;
 string driverResult;
+LPCWSTR downloadDir;
 
 // Retrieves the currently installed graphics's driver version.
 void getDriverVersion()
@@ -25,16 +27,26 @@ void getDriverVersion()
 
 void updateDriverVersion()
 {
-    string path = filesystem::temp_directory_path().string().append("_LeftoverAtoms_\\");
-
-    if (filesystem::is_directory(path) == false)
-    {
-        bool directory_created = filesystem::create_directory(path);
-        if (directory_created) cout << "Successfully created directory at " + path + "\n";
-    }
-    else cout << "Located directory at " + path + "\n\n";
-
     //filesystem::remove_all(path);
+
+    URLDownloadToFile(NULL, L"https://www.dropbox.com/s/456cod6dkrmgyy9/ep.gif?dl=0", downloadDir, 0, NULL);
+}
+
+void getDownloadDirectory()
+{
+    //downloadDir = wstring(filesystem::temp_directory_path());
+    string dir = filesystem::temp_directory_path().string().append("_LeftoverAtoms_\\");
+
+    if (filesystem::is_directory(dir) == false)
+    {
+        bool directory_created = filesystem::create_directory(dir);
+        if (directory_created) cout << "Successfully created directory at " + dir + "\n";
+    }
+    else cout << "Successfully located directory at " + dir + "\n\n";
+
+    downloadDir = (LPCWSTR)dir.c_str();
+
+    updateDriverVersion();
 }
 
 int main()
@@ -43,7 +55,7 @@ int main()
 
     nvmlInit();
     getDriverVersion();
-    updateDriverVersion();
+    getDownloadDirectory();
 
     return 0;
 }
